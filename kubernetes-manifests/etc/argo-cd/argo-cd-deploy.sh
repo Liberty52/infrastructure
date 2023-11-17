cfd1b95d424b1979f8b8af581fb8c96c07b7c1dc
ImKunYoung <ur2kunyoung2@outlook.com>
2023-11-18 02:27:51 +0900

    network(#44): Update argo-cd-deploy.sh

--- kubernetes-manifests/etc/argo-cd/argo-cd-deploy.sh
+++ kubernetes-manifests/etc/argo-cd/argo-cd-deploy.sh
@@ -1,9 +1,12 @@
 # Install Argo CD
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd -n argocd --set server.extraArgs='{--insecure,--grpc-web}'
 
 # argocd-server 밖에 노출
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 sleep 120
 
 # 패스워드 확인
